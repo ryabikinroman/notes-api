3ce60e4b7c65a5f2e8dbb6c9aede910d1bd5b3fc
Ryabikin Roman <ryabikinroman33@gmail.com>
2025-11-10 20:31:56 +0300

    I've finished writing it to the minimum limit, I'm running the first tests.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module notes-api
 go 1.24.4
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+
+	"notes-api/internal/handlers"
+	"notes-api/internal/storage"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Сервер запущен")
-}
-
 func main() {
-	http.HandleFunc("/notes", handler)
-	fmt.Println("Сервер запущен на http://localhost:8080/notes")
-	http.ListenAndServe(":8080", nil)
+	if err := godotenv.Load(); err != nil {
+		log.Println("Не удалось загрузить .env — используем системные переменные")
+	}
+
+	db, err := storage.NewDB()
+	if err != nil {
+		log.Fatalf("Ошибка подключения к БД: %v", err)
+	}
+	defer db.Close()
+
+	h := &handlers.Handler{DB: db}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/notes", h.GetAllNotesHandler).Methods("GET")
+	r.HandleFunc("/notes", h.CreateNoteHandler).Methods("POST")
+	r.HandleFunc("/notes/{id}", h.GetNoteByIDHandler).Methods("GET")
+	r.HandleFunc("/notes/{id}", h.UpdateNoteHandler).Methods("PUT")
+	r.HandleFunc("/notes/{id}", h.DeleteNoteHandler).Methods("DELETE")
+
+	addr := os.Getenv("LISTEN_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	log.Printf("Сервер запущен на http://localhost%s", addr)
+	log.Fatal(http.ListenAndServe(addr, r))
 }
